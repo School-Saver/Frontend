c340d7c3474d91da87f1577dc05f957b74831c83
she11fish <trolltrolltrolllollollol@outlook.com>
2023-01-07 18:46:34 -0500

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />}/>
-        <Route path="*" element={<Navigate to="/" replace />} />
+        <Route element={<Home />} />
         <Route path="/notes" element={<Notes />} />
         <Route path="/bookmarks" element={<Bookmarks />}/>
       </Routes>
