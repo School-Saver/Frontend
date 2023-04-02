90b76268024f85ac00c9362c4b21e9f1fe815616
she11fish <trolltrolltrolllollollol@outlook.com>
2023-04-02 07:57:46 -0400

    changing URL

--- src/utils/util.ts
+++ src/utils/util.ts
@@ -3,7 +3,7 @@ import { BookmarkRow } from "../interfaces/interface";
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "firebase/auth"
 import { auth } from "../firebase-config";
 
-const URL = "https://frontend-git-main-school-saver.vercel.app/"
+const URL = "https://frontend-git-main-school-saver.vercel.app"
 
 export async function createBookmark(bookmark: string, link: string) {
     try {
