0d0a7840314e16ff2c3651ba8f6f4c5790d26fb4
she11fish <trolltrolltrolllollollol@outlook.com>
2023-04-02 08:14:03 -0400

    changing URL

--- src/utils/util.ts
+++ src/utils/util.ts
@@ -3,7 +3,7 @@ import { BookmarkRow } from "../interfaces/interface";
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "firebase/auth"
 import { auth } from "../firebase-config";
 
-const URL = "https://ubiquitous-bonbon-316cbf.netlify.app"
+const URL = "https://backend-production-3c77.up.railway.app"
 
 export async function createBookmark(bookmark: string, link: string) {
     try {
